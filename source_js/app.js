0e662de3c393ef65af340325ec8d72499c5f17cf
DandiW <dwang15@icloud.com>
2016-03-06 01:50:37 -0600

    initial commit

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,23 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
+	$routeProvider
+		.when('/gallery', {
+			templateUrl: 'partials/gallery.html',
+			controller: 'galleryController'
+		})
+		.when('/list', {
+			templateUrl: 'partials/list.html',
+			controller: 'listController'
+		})
+        .when('/detail/:rank', {
+                templateUrl: 'partials/detail.html',
+                controller: 'detailController'
+            })
+		.otherwise({
+			redirectTo:'/gallery'
+		});
 })
+
+
+
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,52 @@
 /* Sample Controller */
+
+/* Sample Controller */
+app.controller('galleryController', ['$scope', '$http', 
+function($scope, $http){
+//	$scope.myName = "dandi Khandekar";
+//
+//	$scope.classdata = {
+//		"title":"CS498RK",
+//		"names": ["777", "b", "c", "d", "e"]
+//	};
+
+	$scope.change = function() {
+		$scope.classdata.names.push($scope.inputValue);
+		$scope.inputValue = "";
+	}
+
+	$http.get('data/imdb250.json')
+        .success(function (data){
+			$scope.myFile = data;
+            //$scope.inputValue = "";
+			console.log(data);
+	       })
+        .error(function (err){
+			console.log(err);
+	})
+    
+
+
+}])
+
+
+app.controller('listController', ['$scope', '$http', function($scope, $http){
+
+    
+    $http.get('data/imdb250.json')
+        .success(function (data){
+			$scope.myFile = data;
+			console.log(data);
+	       })
+        .error(function (err){
+			console.log(err);
+	})
+
+}])
+
+app.controller('detailController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams){
+
+	$scope.mytitle = $routeParams.title;
+	console.log($routeParams.rank);
+
+}])
